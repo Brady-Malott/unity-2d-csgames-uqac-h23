24a32117af874b85f945ad3eaa72d3cf867a55a7
Participant <BIG Z>
2023-03-18 10:27:32 -0400

    enemy follows

--- Assets/Game/Enemy/Enemy.cs
+++ Assets/Game/Enemy/Enemy.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform target;
+    public float speed = 2f;
+    private float minDistance = 1f;
+    private float range;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        range = Vector2.Distance(transform.position, target.position);
         
+        if (range > minDistance)
+        {
+            Debug.Log(range);
+
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
     }
 }
